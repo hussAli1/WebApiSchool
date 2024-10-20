dca1ca3e03bcd92d0f56dda9b3d31efed3328493
hussAli1 <hussainnonhussain@gmail.com>
2024-10-20 17:23:00 +0300

    i

---
+++ WebApiSchool/DataAccess/Entities/AuditLog.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using WebApiSchool.DataAccess.Models;
+
+namespace WebApiSchool.DataAccess.Entities
+{
+    public class AuditLog
+    {
+        [Display(Name = "رقم العملية")]
+        public int AuditId { get; set; }
+
+        [Display(Name = "اسم الجدول ")]
+        public string TableName { get; set; }
+        public string TablePK { get; set; }
+
+        [Display(Name = "العملية")]
+        public string AuditAction { get; set; }
+
+        [Display(Name = "الوقت والتاريخ")]
+        public DateTime AuditDateTime { get; set; }
+
+        [Display(Name = "عنوان الشبكة")]
+        public string AuditPcIp { get; set; }
+
+        [Display(Name = "اسم الحاسبة")]
+        public string AuditPcName { get; set; }
+
+        [Display(Name = "المستخدم")]
+        public User AuditUser { get; set; }
+
+        public string AuditData { get; set; }
+    }
+}
